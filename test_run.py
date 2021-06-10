2e0ddfbccdc9a4941e1e275d21d7bc754427a63d
lostleaf <lostleaf@me.com>
2021-06-10 20:24:24 +0800

    Update test code

--- test_run.py
+++ test_run.py
@@ -3,16 +3,13 @@ import numpy as np
 from binance_tick_parser import *
 import binance_tick_parser.ext as ext
 
-data = read_snapshots('sample_data/BTCUSDT_T_DEPTH_2021-06-05_depth_snap.csv.zst', SnapshotType.FULL)
+data = read_snapshots('sample_data/BTCUSDT_T_DEPTH_2021-06-02_depth_snap.csv.zst', SnapshotType.FULL)
 print(data.shape)
 print(data.dtype)
-ext.test(data)
+ext.show_array_info(data)
 
-data, last_full_snapshot = read_updates('sample_data/BTCUSDT_T_DEPTH_2021-06-05_depth_update.csv.zst',
-                                       data[0],
-                                       SnapshotType.SIMPLIFIED,
-                                       last_full=True)
+p = 'sample_data/BTCUSDT_T_DEPTH_2021-06-02_depth_update.csv.zst'
+data, last_full_snapshot = read_updates(p, data, SnapshotType.SIMPLIFIED, last_full=True)
 print(data.shape)
 print(data.dtype)
-print(convert_to_dataframe(data))
 print(convert_to_dataframe(last_full_snapshot.reshape((-1,))).iloc[0])
